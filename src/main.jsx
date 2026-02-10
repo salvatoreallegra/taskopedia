48499c2b560bb24f6c59a9ef09d100a16f634313
Salvatore Allegra <salallegra@gmail.com>
2026-02-10 13:34:49 -0800

    split into 2 components, render in root

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+function Hello() {
+  return <h1>Hello, world!</h1>;
+}
+function Footer() {
+  return <footer>Copyright 2024</footer>;
+}
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <h1>Hello, world!</h1>
+    <Hello />
+    <Footer />
   </StrictMode>,
 );
